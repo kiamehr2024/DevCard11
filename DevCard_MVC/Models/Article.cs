450c2c0a8ba13a3ba47a03ebbf3e3d76c44e0531
kiamehr askari <kiaaskari80@gmail.com>
2024-09-17 11:57:12 +0330

    Project section from index page moved to partial Project

---
+++ DevCard_MVC/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿namespace DevCard_MVC.Models
+{
+	public class Article
+	{
+		public long Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+	}
+	public class IndexModel
+	{
+		public List<Project> Projects { get; set; }
+		public List<Article> Articles { get; set; }
+	}
+}
---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿namespace DevCard_MVC.Models
+{
+	public class Project
+	{
+		public string Title { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Client { get; set; }
+	}
+
+	
+
+	//public class IndexModel
+	//{
+	//	public List<Project> Projects { get; set; }
+	//	public List<Article> Articles { get; set; }
+	//}
+}
