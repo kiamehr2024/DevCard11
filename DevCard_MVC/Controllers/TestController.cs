cbeb6822526bfc39a066963dc17333a194068ec4
kiamehr askari <kiaaskari80@gmail.com>
2024-09-18 09:49:48 +0330

    Contact for client side validation implemented

---
+++ DevCard_MVC/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PartialView("Footer");
+        }
+    }
+}
